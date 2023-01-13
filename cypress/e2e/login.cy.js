8625f1e7027a5583df1d69b9efc1eff4ad3016fc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-13 13:51:47 -0500

    comments

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,63 @@
+// https://github.com/bahmutov/cypress-data-session
+// import the cypress-data-session plugin
+// to use the cy.dataSession command
 import 'cypress-data-session'
 
 beforeEach(() => {
-  const username = `cy-${Cypress._.random(1e6)}`
-  const email = username + '@acme.co'
-  const password = `!secret-${Cypress._.random(1e6)}`
-  cy.visit('/')
-  cy.contains('a', 'Sign up').click()
-  cy.location('pathname').should('equal', '/register')
-
-  cy.get('input[placeholder=Username]').type(username)
-  cy.get('input[placeholder=Email]').type(email)
-  cy.get('input[placeholder=Password]').type(password)
-  cy.contains('button', 'Sign up').click()
-  cy.location('pathname').should('equal', '/')
-  // we should be logged in
-  cy.contains('a.nav-link', username).should('be.visible')
+  cy.dataSession({
+    name: 'user',
+    setup() {
+      // every test can register the user by filling out the form
+      const username = `cy-${Cypress._.random(1e6)}`
+      const email = username + '@acme.co'
+      const password = `!secret-${Cypress._.random(1e6)}`
+      // because the routing in this application is client-side
+      // we cannot directly visit the /register page
+      // and instead must go from the home page
+      cy.visit('/')
+      cy.contains('a', 'Sign up').click()
+      cy.location('pathname').should('equal', '/register')
+      // fill the sign up form
+      cy.get('input[placeholder=Username]').type(username)
+      cy.get('input[placeholder=Email]').type(email)
+      cy.get('input[placeholder=Password]').type(password)
+      cy.contains('button', 'Sign up').click()
+      // we should end up back on the home page
+      cy.location('pathname').should('equal', '/')
+      // and we should be logged in
+      cy.contains('a.nav-link', username).should('be.visible')
+      // how does the page "stay" logged in?
+      // inspect the DevTools application tab to find out
+      // Can you preserve the main information in the data session:
+      // the username, the email, the password, and whatever
+      // information is necessary to recreate the logged in state
+      cy.window()
+        .its('localStorage')
+        .invoke('getItem', 'jwtToken')
+        .then((token) => {
+          cy.wrap({
+            username,
+            email,
+            password,
+            token,
+          })
+        })
+    },
+    // if you have the information saved above,
+    // how do you "reset" the logged in state before visiting the page?
+    // so you end up logged in
+    recreate({ token }) {
+      cy.window().its('localStorage').invoke('setItem', 'jwtToken', token)
+      cy.visit('/')
+    },
+  })
 })
 
-it('is logged in', () => {})
+// the test should start at the home page
+// and immediately be logged in
+it('is logged in', function () {
+  cy.location('pathname').should('equal', '/')
+  // we can check if the user is logged in by confirming
+  // the profile navigation link is there
+  cy.contains('a.nav-link', this.user.username).should('be.visible')
+})
